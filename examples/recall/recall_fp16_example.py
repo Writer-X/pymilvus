bac4a023f8fa363f4f8583552f8dfedb4d729b54
Writer-X <1256866856@qq.com>
2023-12-20 09:50:56 +0800

    Add float16 vector approve
    Signed-off-by: Writer-X <1256866856@qq.com>

---
+++ examples/recall/recall_fp16_example.py
@@ -0,0 +1,129 @@
+import h5py
+import numpy as np
+import time 
+import random
+
+from pymilvus import (
+    connections,
+    list_collections,
+    FieldSchema, CollectionSchema, DataType,
+    Collection, Partition,
+    utility
+)
+
+# def gen_fp16_vectors(num, dim):
+#     raw_vectors = []
+#     fp16_vectors = []
+#     for _ in range(num):
+#         raw_vector = [random.random() for _ in range(dim)]
+#         raw_vectors.append(raw_vector)
+#         fp16_vector = np.array(raw_vector, dtype=np.float16).view(np.uint8).tolist()
+#         fp16_vectors.append(bytes(fp16_vector))
+#     return raw_vectors, fp16_vectors
+# _, vectors = gen_fp16_vectors(10000, 128)
+
+dim = 128
+collection_name = "siftFp16"
+
+# configure milvus hostname and port
+print(f"\nCreate connection...")
+connections.connect(host="localhost", port=19530)
+
+# List all collection names
+print(f"\nList collections...")
+collection_list = list_collections()
+print(list_collections())
+
+if(collection_list.count(collection_name)):
+    print(collection_name, " exist, and drop it")
+    collection = Collection(collection_name)
+    collection.drop()
+    print("drop")
+
+field1 = FieldSchema(name="id", dtype=DataType.INT64, description="int64", is_primary=True)
+field2 = FieldSchema(name = "vec", dtype = DataType.FLOAT16_VECTOR, description = "float16 vector", dim = dim, is_primary = False)
+schema = CollectionSchema(fields = [field1, field2], description = "sift decription")
+collection = Collection(name = collection_name, data = None, schema = schema, shards_num = 2)
+
+print(list_collections())
+
+print(f"\nList partitions...")
+print(collection.partitions)
+
+# insert
+fname = "sift-128-euclidean.hdf5"
+f = h5py.File(fname, 'r')
+data = f['train']
+test = f['test']
+neighbors = f['neighbors']
+print(data.shape)
+print(neighbors.shape)
+print(test.shape)
+
+print("begin insert...")
+counter = 0
+block_num = 100
+block_size = int(data.shape[0]/block_num)
+start = time.time()
+for t in range(block_num):
+    print("inserting: ", counter, " to ", counter + block_size )
+    entities = [
+            [i for i in range(counter, counter + block_size)],
+            # [vectors[i] for i in range(counter, counter + block_size)]
+            [bytes(vec.astype(np.float16).view(np.uint8).tolist()) for vec in data[counter: counter + block_size]]
+            ]
+    insert_result =  collection.insert(entities)
+    print(insert_result)
+    counter = counter + block_size
+print ("end of insert, cost: ", time.time()-start)
+# flush inside this function
+collection.flush()
+print(collection.num_entities)
+
+# create index
+print(f"\nCreate index...")
+# collection.create_index(field_name="vec",
+#         index_params={'index_type': 'FLAT',  
+#             'metric_type': 'L2',
+#             'params': {
+#                 'nlist': 128
+#                 }})
+collection.create_index(field_name="vec",
+        index_params={'index_type': 'IVF_FLAT',  
+            'metric_type': 'L2',
+            'params': {
+                'nlist': 128
+                }})
+
+# load
+print(f"\nLoad...")
+collection.load()
+
+# search
+print(f"\nSearch...")
+res = collection.search([bytes(vec.astype(np.float16).view(np.uint8).tolist()) for vec in test[0:10]],
+                        "vec", {"metric_type": "L2"}, limit=100)
+# res = collection.search([vectors[i] for i in range(10)],
+#                         "vec", {"metric_type": "L2"}, limit=100)
+print(res)
+
+# build run neighbors
+run_neighbors = []
+for i in range(10):
+    run_neighbors.append(res[i].ids)
+print("run_neighbors: ", run_neighbors)
+
+def get_recall_values(dataset_neighbors, run_neighbors, nq, count):
+    recalls = 0.0
+    for i in range(len(run_neighbors[:nq])):
+        inter = np.intersect1d(run_neighbors[i][:count], dataset_neighbors[i][:count])
+        recalls += inter.shape[0] / count
+    return recalls / nq
+
+# recall
+print(f"\nRecall...")
+
+recall_value = get_recall_values(neighbors, run_neighbors, 10, 100)
+print("recall: ", recall_value)
+
+
